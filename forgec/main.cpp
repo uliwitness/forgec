7b7c9c370f699f994c2420e8ee7a349e843b1fc9
Uli Kusterer <ulivc@zathras.de>
2018-12-11 20:28:25 +0100

    Initial Commit

---
+++ forgec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  forgec
+//
+//  Created by Uli Kusterer on 11.12.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
